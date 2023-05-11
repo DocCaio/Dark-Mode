52365fab0d14315b3641153de2c33c898cb4783c
Caio <108128119+DocCaio@users.noreply.github.com>
2023-05-11 09:01:54 -0300

    update

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 document.querySelector("#btn").addEventListener("click", () => {
     document.body.classList.toggle("dark-mode")
+
+    let paragrafo = document.querySelector('p')
+document.body.classList.contains('dark-mode')
+? paragrafo.textContent = 'Come to the dark side'
+: paragrafo.textContent = 'May the force be with you'
+
 })
